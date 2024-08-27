47dec799dc07407abdeea85c13dd24c8a969121f
Kamran Zaman <kamranzaman0502@gamil.com>
2024-08-27 19:25:23 +0800

    starting the code of streamlitapp

---
+++ StreamlitAPP.py
@@ -0,0 +1,13 @@
+import os
+import json
+import traceback
+import pandas as pd
+from dotenv import load_dotenv
+
+import streamlit as st
+from langchain.callbacks import get_openai_callback
+
+from src.mcqgenerator.utils import read_file, get_table_data
+from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
+from src.mcqgenerator.logger import logging
+
